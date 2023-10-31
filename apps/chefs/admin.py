466d4746e5b97c444511e2ccc0efeaf167765868
Christos Marroquin <christos.marroquin@hotmail.com>
2023-10-30 23:10:24 -0600

    admin site

--- apps/chefs/admin.py
+++ apps/chefs/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Chef
 
+
 # Register your models here.
-admin.site.register(Chef)
+class ChefAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_display=["name", "last_name"]
+
+
+admin.site.register(Chef, ChefAdmin)
--- apps/recipes/admin.py
+++ apps/recipes/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Recipe
 
+
 # Register your models here.
-admin.site.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_display = ["name", "chef"]
+
+
+admin.site.register(Recipe, RecipeAdmin)
